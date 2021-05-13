6335922d53210c35af5b4cb31d11cbce48296094
Geoxor <geoxor@HP-Kali.familiab.com>
2021-05-13 18:09:07 -0500

    bug que crea un elemento vacio *Solucionado*

--- js/model.js
+++ js/model.js
@@ -3,14 +3,7 @@ export default class Model{
         this.view = null;
         this.todos = JSON.parse(localStorage.getItem('todos'));
         if(!this.todos || this.todos.length < 1){
-            this.todos = [
-                {
-                    // id: 0,
-                    // title: 'Learn JS',
-                    // description: 'Whatch JS tutorials',
-                    // completed: false,
-                }
-            ]
+            this.todos = []
             this.currentId = 1;
         }else{
             this.currentId = this.todos[this.todos.length - 1].id + 1;
